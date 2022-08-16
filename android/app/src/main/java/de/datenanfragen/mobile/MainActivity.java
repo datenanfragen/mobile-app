006a1aa01e264ed92bd7d789d3f396c9b405fc13
Benjamin Altpeter <benjamin@oldpeter.de>
2022-08-16 15:20:15 +0200

    Add Android Capacitor plugin for SMTP and basic IMAP

--- android/app/src/main/java/de/datenanfragen/mobile/MainActivity.java
+++ android/app/src/main/java/de/datenanfragen/mobile/MainActivity.java
@@ -1,5 +1,13 @@
 package de.datenanfragen.mobile;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import de.datenanfragen.mobile.plugins.Email.EmailPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(EmailPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
